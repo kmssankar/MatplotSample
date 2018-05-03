e5f4fd8def3fc77d8991b1c93fcee52de9f87391
kmssankar <s.sankarganesh@hotmail.com>
2018-05-03 23:17:28 +0530

    Initial Commit - Sankar

---
+++ TensorTest/Tuturial01.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as Mplot
+# Create Numpy arange of values for X  and Y Axis
+Xseries = np.arange(1,12,2);
+Yseries = np.arange(1,24,4);
+
+#Plot X and Y Axis
+Mplot.plot(Xseries,Yseries);
+#Show the Plot
+Mplot.show();
+
+
+
+
+
+
